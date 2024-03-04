319cf19bce465b09bfa79d35ff24d25930b0c67f
Erik Achtermann <erik_a@comcast.net>
2024-03-03 18:28:44 -0600

    dont pull merge

--- Assets/Scripts/NetworkCollisions.cs
+++ Assets/Scripts/NetworkCollisions.cs
@@ -1,56 +1,133 @@
-using UnityEngine;
+using Photon.Pun;
 using System.Collections;
+using UnityEngine;
+using System;
 
-public class NetworkCollisions : MonoBehaviour
+
+public class TaggingScript : MonoBehaviour
 {
-    bool hasCollided;
-    private bool isIt;
-    float collisionCooldown = 5.0f;
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.name == "Body" && !hasCollided)
-        {
-            //Debug.Log("Collision detected with: " + collision.gameObject.name);
-            
-            isIt = transform.parent.transform.GetChild(1).gameObject.GetComponent<DisplayRoleScript>().get_is_tagger();
-            Debug.Log("isIt = " + isIt);
-            hasCollided = true;
-            if (isIt)
-            {
-                //isIt = false;
-                //collision.gameObject.GetComponent<NetworkCollisions>().isIt = true;
-                transform.parent.transform.GetChild(1).gameObject.GetComponent<DisplayRoleScript>().change_role();
-                GameObject colUI = collision.gameObject.transform.parent.transform.GetChild(1).gameObject;
-                Debug.Log("collision role = " + colUI.GetComponent<DisplayRoleScript>().get_is_tagger());
-                colUI.GetComponent<DisplayRoleScript>().change_role();
-                colUI.GetComponent<DisplayRoleScript>().lose_life();
-                Debug.Log("collision role = " + colUI.GetComponent<DisplayRoleScript>().get_is_tagger());
-                FreezePlayer(collision.gameObject.GetComponent<Rigidbody>());
-            }
-            StartCoroutine(ResetCollisionFlag());
-            StartCoroutine(UnfreezePlayer(collision.gameObject.GetComponent<Rigidbody>()));
+    private int startingLives = 3;
+    private int currentLives;
+    private bool collisionCooldownActive = false;
+    private float collisionCooldownDuration = 5f;
+    private bool currentlyIt;
 
+    private PhotonView myView;
+
+
+    private void Start()
+    {
+        myView = transform.parent.gameObject.GetComponent<PhotonView>();
+        Debug.Log(myView.ViewID);
+        if (myView.IsMine)
+        {
+            currentLives = startingLives;
+            if (PhotonNetwork.CountOfPlayers == 1)
+            {
+                Debug.Log("Player " + PhotonNetwork.CountOfPlayers + " is it and has id " + myView.ViewID);
+                currentlyIt = true;
+            }
+            else
+            {
+                Debug.Log("Player " + PhotonNetwork.CountOfPlayers + " is not it");
+                currentlyIt = false;
+            }
         }
     }
-    private IEnumerator ResetCollisionFlag()
+
+    private void OnCollisionEnter(Collision collision)
     {
-        //Debug.Log("countdown begin");
-        yield return new WaitForSeconds(collisionCooldown);
-        //Debug.Log("countdown end");
-        hasCollided = false;
-    }
-    private void FreezePlayer(Rigidbody rb)
-    {
-        rb.constraints = RigidbodyConstraints.FreezePosition;
+        if (!collisionCooldownActive && collision.gameObject.name == "Body") 
+        {
+            int pvid = collision.gameObject.GetComponent<TaggingScript>().getMyView().ViewID;
+            bool beingTagged = false;
+            if (currentlyIt)
+            {
+                beingTagged = true;
+            }
+            // Debug.Log(collision.gameObject.GetPhotonView().gameObject.name + " has id " + collision.gameObject.GetPhotonView().ViewID);
+            // if (collision.gameObject.name == "Body")
+            // {
+            //     Debug.Log("Collided with " + collision.gameObject.GetComponent<TaggingScript>().getMyView().ViewID);
+            //     if (currentlyIt)
+            //     {
+            //         Debug.Log(myView.ViewID + " is tagging " + collision.gameObject.GetComponent<TaggingScript>().getMyView().ViewID);
+                        myView.RPC("TagPlayer", RpcTarget.All, pvid, beingTagged);
+            //         currentlyIt = false;
+            //         Debug.Log(myView.ViewID + " currentlyIt is now " + currentlyIt);
+            //     }
+            // }
+            StartCoroutine(ActivateCollisionCooldown());
+        }
     }
 
-    IEnumerator UnfreezePlayer(Rigidbody rb)
+    private IEnumerator ActivateCollisionCooldown()
     {
-        yield return new WaitForSeconds(collisionCooldown);
+        collisionCooldownActive = true;
+        yield return new WaitForSeconds(collisionCooldownDuration);
+        collisionCooldownActive = false;
+    }
 
+    [PunRPC]
+    void TagPlayer(int pvid, bool beingTagged)
+    {
+        if (myView.IsMine)
+        {
+            if (currentlyIt)
+            {
+                currentlyIt = false;
+            }
+
+            else if (myView.ViewID == pvid && beingTagged)
+            {
+                LoseLife();
+                currentlyIt = true;
+                FreezePlayer(collisionCooldownDuration);
+            }
+        // Debug.Log("Tagging");
+        // LoseLife();
+        // ChangeRoleToTagged();
+        // FreezePlayer(collisionCooldownDuration);
+        }
+    }
+
+    private void LoseLife()
+    {
+        currentLives--;
+        if (currentLives <= 0)
+        {
+            Debug.Log("Game Over");
+        }
+        // Update UI or other actions related to life loss
+    }
+
+    private void ChangeRoleToTagged()
+    {
+        currentlyIt = true;
+        Debug.Log(myView.ViewID + " currentlyIt is now " + currentlyIt);
+        // Implement role change to tagged and update UI if necessary
+    }
+
+    private void FreezePlayer(float duration)
+    {
+        StartCoroutine(FreezeCoroutine(duration));
+    }
+
+    private IEnumerator FreezeCoroutine(float duration)
+    {
+        Rigidbody rb = GetComponent<Rigidbody>(); // Assuming the player has a Rigidbody
+        rb.constraints = RigidbodyConstraints.FreezeAll;
+        yield return new WaitForSeconds(duration);
         rb.constraints = RigidbodyConstraints.None;
     }
 
+    public PhotonView getMyView()
+    {
+        return myView;
+    }
+
+    public bool getCurrentlyIt ()
+    {
+        return currentlyIt;
+    }
 }
-
-
